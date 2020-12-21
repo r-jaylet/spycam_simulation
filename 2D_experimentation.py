97505e06e568ebb0a7715b0299d74d527d41710d
remij <remi.jaylet@hotmail.com>
2020-12-21 12:08:07 +0100

    add files

---
+++ 2D_experimentation.py
@@ -0,0 +1,325 @@
+import matplotlib.pyplot as plt
+from numpy import *
+from random import random
+from scipy import misc 
+
+'CONTEXTE'
+
+D=0.75 #distance séparant les deux poulies [m]
+H=0.8 #hauteur des deux poulies [m]
+m=0.18 #masse de la caméra [kg]
+g=9.81 #acc de la pesanteur [m/s^2]
+r=0.028 #rayon de poulie
+
+A1=[0,H]
+A2=[D,H]
+
+L10=((A1[0])**2+(A1[1])**2)**(1/2)
+L20=((A2[0])**2+(A2[1])**2)**(1/2)
+
+T=arange(0,2*pi,0.1)
+
+
+'MODELE GEOMETRIQUE DIRECT'
+
+def th1(M):
+    return -arctan(((A1[1]-M[1])/(A1[0]-M[0])))
+
+def th2(M):
+    return arctan((A2[1]-M[1])/(A2[0]-M[0]))
+
+def lo1(M): 
+    return ((M[0]-A1[0])**2+(M[1]-A1[1])**2)**(1/2)
+
+def lo2(M):
+    return ((M[0]-A2[0])**2+(M[1]-A1[1])**2)**(1/2)
+
+def tens(M):
+    return [(m*g*cos(th2(M)))/sin(th1(M)+th2(M)),(m*g*cos(th1(M)))/sin(th1(M)+th2(M))]
+
+T1=tens([0.37,0.64])
+print(T1)
+
+
+'definition trajectoire circulaire'
+C=[0.25,0.25]
+R=0.10
+
+X=C[0]+R*cos(T)
+Y=C[1]+R*sin(T)
+M=[X,Y]
+
+VX=-R*sin(T)
+VY=R*cos(T)
+AX=-R*cos(T)
+AY=-R*sin(T)
+
+
+'visualisation trajectoire'
+
+#p1=plt.plot(T,X)
+#p2=plt.plot(T,Y)
+#p3=plt.plot(X,Y)
+#plt.axis([0,0.55,0,0.64])
+#plt.show()
+
+#p1=plt.plot(T,VX)
+#p2=plt.plot(T,VY)
+#plt.show()
+
+#p1=plt.plot(T,AX)
+#p2=plt.plot(T,AY)
+#plt.show()
+
+
+
+'EVOLUTION DES ANGLES' 
+
+def thetadegre(M):
+    t1=[]
+    t2=[]
+    for i in range(len(T)):
+        t1.append(-180*(arctan((A1[1]-M[1][i])/(A1[0]-M[0][i])))/3.14)
+        t2.append(180*(arctan((A2[1]-M[1][i])/(A2[0]-M[0][i])))/3.14)
+    return [t1,t2]
+
+thetaz1=(thetadegre(M))[0]
+thetaz2=(thetadegre(M))[1]
+
+#p1=plt.plot(T,thetaZ1)
+#p2=plt.plot(T,thetaz2)
+#plt.show()
+    
+def theta(M):
+    t1=[]
+    t2=[]
+    for i in range(len(T)):
+        t1.append(-arctan((A1[1]-M[1][i])/(A1[0]-M[0][i]))) #attention signe pour derivées
+        t2.append(arctan((A2[1]-M[1][i])/(A2[0]-M[0][i])))
+    return [t1,t2]
+
+theta1=(theta(M))[0]
+theta2=(theta(M))[1]
+
+#p1=plt.plot(T,theta1)
+#p2=plt.plot(T,theta2)
+#plt.show()
+
+
+
+'EVOLUTION DES GRANDEURS CARACtERISTIQUES'
+
+def longueur(M):
+    L1=[]
+    L2=[]
+    for i in range(len(T)):
+        L1.append(((M[0][i]-A1[0])**2+(M[1][i]-A1[1])**2)**(1/2))
+        L2.append(((M[0][i]-A2[0])**2+(M[1][i]-A1[1])**2)**(1/2))
+    return [L1,L2]
+
+L1=(longueur(M))[0]
+L2=(longueur(M))[1] 
+
+#print(L1)
+#print(L2)
+
+#p1=plt.plot(T,L1)
+#p2=plt.plot(T,L2)
+#plt.show()
+
+def thetad(M):
+    t1=[]
+    t2=[]
+    for i in range(len(T)):
+        t1.append((((-sin(theta1[i]))*VX[i])/L1[i])+(((cos(theta1[i]))*VY[i])/L2[i]))
+        t2.append((((-sin(theta2[i]))*VX[i])/L2[i])+(((cos(theta2[i]))*VY[i])/L2[i]))
+    return [t1,t2]
+
+thetad1=(thetad(M))[0]
+thetad2=(thetad(M))[1]
+
+#p1=plt.plot(T,thetad1)
+#p2=plt.plot(T,thetad2)
+#plt.show()
+
+
+def longueurd(M):
+    L1=[]
+    L2=[]
+    for i in range(len(T)):
+        L1.append(cos(theta1[i])*VX[i]+sin(theta1[i])*VY[i])
+        L2.append(cos(theta2[i])*VX[i]+sin(theta2[i])*VY[i])
+    return [L1,L2]
+
+Ld1=(longueurd(M))[0]
+Ld2=(longueurd(M))[1] 
+
+#p1=plt.plot(T,Ld1)
+#p2=plt.plot(T,Ld2)
+#plt.show()
+
+
+'EVOLUTION TENSION CABLE'
+
+def tension():
+    F1=[]
+    F2=[]
+    for i in range(len(T)):
+        F1.append((-m*AX[i]*sin(theta2[i])+m*g*cos(theta2[i]))/sin(theta1[i]+theta2[i]))
+        F2.append((m*AX[i]*sin(theta1[i])+m*(g+AX[i])*cos(theta1[i]))/sin(theta1[i]+theta2[i]))
+    return [F1,F2]
+
+Tension1=tension()[0]
+Tension2=tension()[1]
+
+#p1=plt.plot(T,Tension1)
+#p2=plt.plot(T,Tension2)
+#plt.show()
+
+
+
+def plan(a,b):
+    i=[]
+    j=[]
+    for x in arange(0,a,0.005):
+        for y in arange(0,b,0.005):
+            M=[x,y]
+            if (sin(th1(M)+th2(M)))!=0:
+                if (m*g*(cos(th2(M))/sin(th1(M)+th2(M))))>1 or (m*g*(cos(th1(M))/sin(th1(M)+th2(M))))>1:
+                    i.append(M[0])
+                    j.append(M[1])
+    return [i,j]
+
+#X= plan(0.56,0.64)[0]
+#Y= plan(0.56,0.64)[1]
+
+#p2=plt.plot(X,Y)
+#plt.axis([0.05,0.5,0.50,0.64])
+#plt.show()
+
+def plan(seuil):
+    i=[]
+    j=[]
+    for x in arange(0,D,0.003):
+        for y in arange(0,H,0.003):
+            M=[x,y]
+            if (sin(th1(M)+th2(M)))!=0:
+                if (m*g*(cos(th2(M))/sin(th1(M)+th2(M))))>seuil or (m*g*(cos(th1(M))/sin(th1(M)+th2(M))))>seuil:
+                    i.append(M[0])
+                    j.append(M[1])
+    k=100*(H-min(j))/H
+    return [i,j,k]
+
+X= plan(5)[0]
+Y= plan(5)[1]
+
+p2=plt.plot(X,Y)
+plt.axis([0,D,0.5,H])
+plt.show()
+
+print(plan(5)[2])
+
+    
+    
+'EVOLUTION ROTATION DES MOTEURS'
+
+def beta(M):
+    b1=[]
+    b2=[]
+    for i in range(len(T)):
+        b1.append((L1[0]-L1[i])/r)
+        b2.append((L2[0]-L2[i])/r)
+    return[b1,b2]
+
+beta1=(beta(M))[0]
+beta2=(beta(M))[1]
+
+#p1=plt.plot(T,beta1)
+#p2=plt.plot(T,beta2)
+#plt.show()
+
+def betad(M):
+    bd1=[]
+    bd2=[]
+    for i in range(len(T)):
+        bd1.append(-(((M[0][i]-A1[0])*VX[i])+((M[1][i]-A1[1])*VY[i]))/(r*L1[i]))
+        bd2.append((((M[0][i]-A2[0])*VX[i])+((M[1][i]-A2[1])*VY[i]))/(r*L2[i]))
+    return[bd1,bd2]
+
+betad1=(betad(M))[0]
+betad2=(betad(M))[1]
+
+#p1=plt.plot(T,betad1)
+#p2=plt.plot(T,betad2)
+#plt.show()
+   
+def betadd(M):
+    bdd1=[]
+    bdd2=[]
+    for i in range(len(T)):
+        bdd1.append(((-(((VX[i])**2+(M[0][i]-A1[0])*AX[i])+(VY[i])**2+(M[1][i]-A1[1])*AY[i])*Ld1[i])/(r*(L1[i])**2))+betad1[i])
+        bdd2.append(((-(((VX[i])**2+(M[1][i]-A2[0])*AX[i])+(VY[i])**2+(M[1][i]-A2[1])*AY[i])*Ld2[i])/(r*(L2[i])**2))+betad2[i])
+    return[bdd1,bdd2]
+
+betadd1=(betadd(M))[0]
+betadd2=(betadd(M))[1]
+
+#p1=plt.plot(T,betadd1)
+#p2=plt.plot(T,betadd2)
+#plt.show()
+
+
+def multi(L,k):
+    A=[]
+    for i in range(len(L)):
+        A.append((k*L[i]))
+    return A
+
+
+vitesseRPM1=multi(betad1,(60/(2*3.14)))
+vitesseRPM2=multi(betad2,(60/(2*3.14)))
+
+#p1=plt.plot(T,vitesseRPM1)
+#p2=plt.plot(T,vitesseRPM2)
+#plt.show()
+
+
+
+'COMMANDE ARDUINO'
+
+def adaptation(vRPM1,vRPM2):
+    L1=[]
+    L2=[]
+    for i in range(len(vRPM1)):
+        if vRPM1[i]<=0:
+            L1.append(int(5.84*vRPM1[i]+1529))
+        elif  vRPM1[i]>0:
+            L1.append(int(5.95*vRPM1[i]+1487))
+        else :
+            L1.append(0)
+        if vRPM2[i]<=0:
+            L2.append(int(5.32*vRPM2[i]+1456.4))
+        elif vRPM2[i]>0 :
+            L2.append(int(5.56*vRPM2[i]+1499.1))
+        else :
+            L2.append(0)
+    return L1,L2
+
+vitessearduino1=adaptation(vitesseRPM1,vitesseRPM2)[0]    
+vitessearduino2=adaptation(vitesseRPM1,vitesseRPM2)[1]
+
+#p1=plt.plot(T,vitessearduino1)
+#p2=plt.plot(T,vitessearduino2)
+#plt.show()
+#print(vitessearduino1)
+#print(vitessearduino2)
+
+
+def arduino(L1,L2):
+    for i in range(len(L1)):
+        print("servo1.writeMicroseconds("+str(L1[i])+");")
+        print("servo2.writeMicroseconds("+str(L2[i])+"); delay(200);")
+
+
+
+#print(arduino(vitessearduino1, vitessearduino2))
---
+++ simulation_functions.py
@@ -0,0 +1,168 @@
+from math import *
+import numpy as np
+import scipy.integrate as spint
+
+
+
+def CalcPXTH(X,STADE,R):
+    """
+    Calcul de la matrice P de changement de base 
+    
+    entrée :    X position (vecteur 3 composantes)
+                STADE taille du stade (vecteur 3 composantes)   
+                R rayon des treuils (scalaire)
+    sortie : matrice 3x3
+    """
+    x=X[0]; y=X[1]; z=X[2]
+    d=STADE[0]; s=STADE[1]; v=STADE[2]
+    k=np.sqrt(x**2+y**2+z**2)
+    h=np.sqrt((d-x)**2+y**2+z**2)
+    m=np.sqrt((d-x)**2+(s-y)**2+z**2)
+    n=np.sqrt(x**2+(s-y)**2+z**2)
+    
+    P=np.zeros((3,3))
+    P[0,0]=-(d-x)/m-(d-x)/h;         P[0,1]=-(s-y)/m+y/h;             P[0,2]=z/m+z/h;
+    P[1,0]=-(d-x)/m+x/n    ;         P[1,1]=-(s-y)/m-(s-y)/n;         P[1,2]=z/m+z/n;
+    P[2,0]=-(d-x)/m+x/n+x/k-(d-x)/h; P[2,1]=-(s-y)/m-(s-y)/n+y/k+y/h; P[2,2]=z/m+z/n+z/k+z/h;
+    
+    return P/R
+
+
+
+def CalcHXTH(X,STADE,R):
+    """
+    Calcul des matrices H1, H2, H3
+    entrée :    X position (vecteur 3 composantes)
+                STADE taille du stade (vecteur 3 composantes)   
+                R rayon des treuils (scalaire)
+    sortie : H1,H2,H3 matrices 3x3
+    """
+    x=X[0]; y=X[1]; z=X[2]
+    d=STADE[0]; s=STADE[1]; v=STADE[0]
+    k=np.sqrt(x**2+y**2+z**2)
+    h=np.sqrt((d-x)**2+y**2+z**2)
+    m=np.sqrt((d-x)**2+(s-y)**2+z**2)
+    n=np.sqrt(x**2+(s-y)**2+z**2)
+    
+    H1=np.zeros((3,3))
+    H1[0,0]=1/m+1/h-((d-x)**2)/m**3-((d-x)**2)/h**3;    H1[0,1]=-(d-x)*(s-y)/m**3+(d-x)*y/h**3;          H1[0,2]=(d-x)*z/m**3+(d-x)*z/h**3;
+    H1[1,0]=-(d-x)*(s-y)/m**3+(d-x)*y/h**3;             H1[1,1]=1/m+1/h-((s-y)**2)/m**3-(y**2)/h**3;     H1[1,2]=(s-y)*z/m**3-y*z/h**3;
+    H1[2,0]=(d-x)*z/m**3+(d-x)*z/h**3;                  H1[2,1]=(s-y)*z/m**3-y*z/h**3;                   H1[2,2]=1/m+1/h-(z**2)/m**3-(z**2)/h**3;
+    
+    H2=np.zeros((3,3))
+    H2[0,0]=1/m+1/n-((d-x)**2)/m**3-(x**2)/n**3;        H2[0,1]=-(d-x)*(s-y)/m**3+x*(s-y)/n**3;          H2[0,2]=(d-x)*z/m**3-x*z/n**3;
+    H2[1,0]=-(d-x)*(s-y)/m**3+x*(s-y)/n**3;             H2[1,1]=1/m+1/n-((s-y)**2)/m**3-((s-y)**2)/n**3; H2[1,2]=(s-y)*z/m**3+(s-y)*z/n**3;
+    H2[2,0]=(d-x)*z/m**3-x*z/n**3;                      H2[2,1]=(s-y)*z/m**3+(s-y)*z/n**3;               H2[2,2]=1/m+1/n-(z**2)/m**3-(z**2)/n**3;
+    
+    H3=np.zeros((3,3))
+    H3[0,0]=1/m+1/n+1/k+1/h-((d-x)**2)/m**3-(x**2)/n**3-(x**2)/k**3-((d-x)**2)/h**3;    H3[0,1]=-(d-x)*(s-y)/m**3+x*(s-y)/n**3-x*y/k**3+(d-x)*y/h**3;                     H3[0,2]=(d-x)*z/m**3-x*z/n**3-x*z/k**3+(d-x)*z/h**3;
+    H3[1,0]=-(d-x)*(s-y)/m**3+x*(s-y)/n**3-x*y/k**3+(d-x)*y/h**3;                       H3[1,1]=1/m+1/n+1/k+1/h-((s-y)**2)/m**3-((s-y)**2)/n**3-(y**2)/k**3-(y**2)/h**3;  H3[1,2]=(s-y)*z/m**3+(s-y)*z/n**3-y*z/k**3-y*z/h**3;
+    H3[2,0]=(d-x)*z/m**3-x*z/n**3-x*z/k**3+(d-x)*z/h**3;                                H3[2,1]=(s-y)*z/m**3+(s-y)*z/n**3-y*z/k**3-y*z/h**3;                              H3[2,2]=1/m+1/n+1/k+1/h-(z**2)/m**3-(z**2)/n**3-(z**2)/k**3-(z**2)/h**3;
+    
+    return H1/R,H2/R,H3/R
+
+
+
+def TrajRef(Xdep,Xfin,tfin,t1,t2,STADE,R,timestep=0.001):
+    """
+    Calcul de la trajectoire de référence
+    entrée :    Xdep position de départ (vecteur colonne 3 composantes)
+                Xfin position d'arrivée (vecteur colonne 3 composantes)
+                tfin date d'arrivée (scalaire)
+                t1 date à la fin de la première accélération (scalaire)
+                t2 date au début du freinage (scalaire)
+                STADE taille du stade (vecteur 3 composantes)
+                R rayon des treuils (scalaire)
+                dt temps d'échantillonnage (scalaire - optionnel)
+    sortie : temps t (vecteur ligne taille N)
+             vitXref position de référence (matrice 3 lignes N colonnes)
+             posXref vitesse de référence (matrice 3 lignes N colonnes)
+             vitThref vitesse angulaire de référence (matrice 3 lignes N colonnes)
+             posThref position angulaire de référence (matrice 3 lignes N colonnes)
+    """
+    Nt=int((tfin-0)/timestep+1)
+    t,dt=np.linspace(0,tfin,Nt,retstep=True)
+
+    V0=2*(Xfin-Xdep)/(tfin+(t2-t1))
+    vitXref=(t<=t1)         *(V0*t/t1)+\
+            ((t>t1)*(t<=t2))*V0       +\
+            (t>t2)          *(-V0/(tfin-t2)*(t-t2)+V0)
+    posXref=(t<=t1)         *(V0*t**2/(2*t1)+Xdep)+\
+            ((t>t1)*(t<=t2))*(V0*(t-t1)+V0*t1/2+Xdep)+\
+            (t>t2)          *(-V0/(2*(tfin-t2))*(t-t2)**2+V0*(t-t1)+V0*t1/2+Xdep)  
+    vitThref=np.zeros((3,Nt))
+    for i in range(Nt):
+        PXth=CalcPXTH(posXref[:,i],STADE,R)
+        vitThref[:,i]=np.dot(PXth,vitXref[:,i])
+        posThref=np.zeros((3,Nt))
+    for k in range(3):
+        posThref[k,:]=spint.cumtrapz(vitThref[k,:],initial=0)*dt 
+
+    return t,vitXref,posXref,vitThref,posThref
+
+
+
+def CalcConsigne(t,time,vitXref,posXref,vitThref,posThref):
+    """"
+    Formation des signaux de consigne pour la régulation par interpolation 
+    à partir du calcul de la trajectoire de référence
+    entrée  : t date à laquelle on souhaite déterminer la consigne (scalaire)
+              time temps (vecteur ligne taille N)
+              vitXref position de référence (vecteur 3 lignes N colonnes)
+              posXref vitesse de référence (vecteur 3 lignes N colonnes)
+              vitThref vitesse angulaire de référence (vecteur 3 lignes N colonnes)
+              posThref position angulaire de référence (vecteur 3 lignes N colonnes)
+    sortie : X_pt0 vitesse de consigne (vecteur 3 colonnes)
+             X0    position de consigne (vecteur 3 colonnes)
+             Th_pt0 vitesse angulaire de consigne (vecteur 3 colonnes)
+             Th0 position angulaire de consigne (vecteur 3 colonnes)
+    """
+    X_pt0=np.zeros((3,1))
+    X0=np.zeros((3,1))
+    Th0=np.zeros((3,1))
+    Th_pt0=np.zeros((3,1))
+    for k in range(3):
+        X_pt0[k]=np.interp(t,time,vitXref[k,:]) 
+        X0[k]=np.interp(t,time,posXref[k,:])
+        Th_pt0[k]=np.interp(t,time,vitThref[k,:]) 
+        Th0[k]=np.interp(t,time,posThref[k,:])
+    return X_pt0,X0,Th_pt0,Th0
+
+
+
+def EqDiff(Y,t,STADE,R,mA,Sinv,L,G,g,Kpos,Kvit,time,vitXref,posXref,vitThref,posThref,Fpert):
+    """
+    Equation différentielle régissant le système  (Y)'=f(Y,t)
+    entrée : Y vecteur 12 composantes 
+             t temps (scalaire)
+             ... tous les arguments nécessaires au calcul
+    sortie : Y' vecteur 12 composantes 
+    
+    """
+    Ypt=np.zeros(12)
+    X   = Y[0:3,None]
+    Xpt = Y[3:6,None]
+    Th  = Y[6:9,None]
+    Thpt= Y[9:12,None]
+    
+    P=CalcPXTH(X,STADE,R)
+    H1,H2,H3=CalcHXTH(X,STADE,R)
+    Xpt0,X0,Thpt0,Th0=CalcConsigne(t,time,vitXref,posXref,vitThref,posThref)
+    
+    M=np.dot(P.transpose(),Sinv)/mA
+    N=np.linalg.inv(np.eye(3)+np.dot(np.dot(M,G),P))
+    A=np.array([[np.dot(np.dot(Xpt.transpose(),H1),Xpt)[0][0]],[np.dot(np.dot(Xpt.transpose(),H2),Xpt)[0][0]],[np.dot(np.dot(Xpt.transpose(),H3),Xpt)[0][0]]])
+    B=np.dot(L,Thpt)
+    C=Kpos*(Th0-Th)
+    D=Kvit*(Thpt0-Thpt)
+    E=-np.array([[0],[0],[g]])+Fpert(t)/mA
+    
+    
+    Ypt[0:3]=Xpt.transpose()
+    Ypt[6:9]=Thpt.transpose()
+    Xsec=np.dot(N,-np.dot(M,A)-np.dot(M,B)+np.dot(M,C)+np.dot(M,D)+E)
+    Thsec=np.dot(P,Xsec)+A
+    Ypt[3:6]=Xsec.transpose()
+    Ypt[9:12]=Thsec.transpose()
+    
+    return Ypt
